13d41e8242498ae9f669f7c0b006e721445692ad
shining013 <nevertheless113@naver.com>
2025-04-07 21:47:40 +0900

    feat: find a node's relatives

---
+++ parents.py
@@ -0,0 +1,25 @@
+import maya.cmds as cmds
+
+def transforms_from_shapes_nodes(shapes):
+    transforms = []
+    
+    for obj in shapes:
+        if cmds.objectType(obj, isAType="shape"):
+            parents = cmds.listRelatives(obj, parent=True)
+            if parents:
+                transforms.append(parents[0])
+    
+    return transforms
+    
+def camera_transforms():
+    camera_shapes = cmds.ls(cameras=True)
+    cameras = transforms_from_shapes_nodes(camera_shapes)
+    
+    return lights
+
+if __name__ == "__main__":
+    lights = camera_transforms()
+    cmds.window(title="Lights")
+    cmds.paneLayout()
+    cmds.textScrollList(append=cameras)
+    cmds.showWindow()
